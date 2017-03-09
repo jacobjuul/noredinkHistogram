cb173f7356fe9093373292144495437dce726a17
Jacob Christiansen <juul@Jacobs-MacBook-Pro.local>
2017-03-09 14:42:37 +0100

    more consice

--- index.js
+++ index.js
@@ -1,54 +1,33 @@
 const YAML = require('yamljs');
 const _ = require('lodash/fp');
-
 const data = YAML.load('./data.yaml');
-//
-// const
-// const females = _.reduce((sum, curr) => {
-//   return Object.assign({}, sum, )
-// }, {})(data);
 
 // getFemales :: Object -> Object
-const getFemales = _.filter((item) => item.bio.gender === 'F');
-
-const filterTerms = _.filter((term) => {
-  return term.type === 'rep';
-});
-
+const getFemales = _.filter(item => item.bio.gender === 'F');
+const filterTerms = _.filter(term => term.type === 'rep');
 const getFemaleReps = _.compose(_.map(_.compose(filterTerms, _.prop('terms'))), getFemales);
-const femaleRepTerms = getFemaleReps(data);
+const getYear = _.compose(_.join(''), _.slice(0,4));
+const getTermsArray = _.compose(_.flatten, getFemaleReps);
 
-const getYear = year => year.slice(0,4);
-
-const getYears = (start, end) => {
+// getYearRange :: String -> String -> [Number]
+const getYearRange = (start, end) => {
   const begin = getYear(start);
   const ending = getYear(end);
-  const diff = +ending - +begin;
+  const diff = ending - begin;
   let ret = [+begin];
 
   _.times(count => {
-    ret.push(+begin + count + 1)
+    ret.push(+begin + (count + 1))
   }, diff);
 
   return ret;
 };
 
-const years = _.flattenDeep(femaleRepTerms.map(terms => {
-  return terms.map(term => {
-    return getYears(term.start, term.end);
-  })
-}));
+// getAllYears :: [Object] -> [Number]
+const getAllYears = _.map(term => getYearRange(term.start, term.end));
 
-const yearCounts = years.reduce((sum, year) => {
-    return Object.assign(
-      {},
-      sum,
-      {
-        [year]: sum[year] ? sum[year] + 1 : 1
-      }
-    );
-}, {})
-
+// getYearsCount :: [Object] -> Object
+const getYearsCount = _.compose(_.countBy(i => i), _.flattenDeep, getAllYears, getTermsArray)
 
 const printHist = data => {
   Object.keys(data).forEach(key => {
@@ -60,20 +39,4 @@ const printHist = data => {
   console.log('\n')
 }
 
-printHist(yearCounts)
-
-
-// const years = femaleReps.reduce((sum, curr) => {
-//
-//   curr.forEach((i) => {
-//
-//   }
-// }, {})
-
-
-// console.log(getRepTerms(data))
-
-// console.log(getRepTerms(data))
-// getTerms :: Object -> Object
-const getStart =  _.prop('terms.start');
-const getEnd = _.prop('terms.end');
+printHist(getYearsCount(data))
