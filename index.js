f6cb90804ba0deeaa407532b7e7d5ffc05a3aa8f
Jacob Christiansen <juul@Jacobs-MacBook-Pro.local>
2017-03-10 16:23:19 +0100

    make shorter

--- index.js
+++ index.js
@@ -2,16 +2,22 @@ const YAML = require('yamljs');
 const _ = require('lodash/fp');
 const data = YAML.load('./data.yaml');
 
+var trace = _.curry(function(tag, x) {
+      console.log(tag, x);
+      return x;
+    });
+
 // getFemales :: Object -> Object
 const getFemales = _.filter(item => item.bio.gender === 'F');
 const filterTerms = _.filter(term => term.type === 'rep');
 const getFemaleReps = _.compose(_.map(_.compose(filterTerms, _.prop('terms'))), getFemales);
 // getTermsArray :: [Object] -> [Object]
 const getTermsArray = _.compose(_.flatten, getFemaleReps);
-// getYear :: [String] ->
+// getYear :: String -> String
 const getYear = _.compose(_.join(''), _.slice(0,4));
 // getYearRange :: Object -> [Number]
-const getYearRange = _.compose(_.map(getYear), _.at(['start', 'end']));
+const getYearRange = _.compose(_.range(_.head, _.init), _.map(getYear), _.at(['start', 'end']));
+
 // getYearsCount :: [Object] -> Object
 const getYearsCount = _.compose(
   _.countBy(i => i),
@@ -20,8 +26,11 @@ const getYearsCount = _.compose(
   getTermsArray
 );
 
-// getBarAsString :: Number -> String
-const printBarAsString = _.compose(console.log, _.join(''), _.times(c => '#'));
-const printBars = _.map(printBarAsString);
-const app = _.compose(printBars, getYearsCount);
+// addLegngth :: String -> String
+const addLength = s => s.length + s;
+// getBarAsString :: Object -> String
+
+const printBars = _.compose(console.log, addLength, _.join(''), _.times(c => '#'));
+const app = _.compose(_.map(printBars), trace('after count'), getYearsCount);
+
 app(data);
